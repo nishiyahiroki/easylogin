3edb410fdf3633993bf2622e125a1586a6bddde4
nishiyahiroki <hirooooo78@icloud.com>
2022-05-31 18:29:10 +0900

    ログイン

---
+++ src/main/java/com/example/easylogin/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.easylogin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.internous.ecsite.model.dao.UserRepository;
+import jp.co.internous.ecsite.model.entity.User;
+import jp.co.internous.ecsite.model.form.UserForm;
+
+@Controller
+@RequestMapping("/ecsite/admin/user")
+public class UserController {
+	@Autowired
+	private UserRepository userRepos;
+	
+	@RequestMapping("/")
+	public String userIndex() {
+		return "register_user";
+	}
+	
+	@PostMapping("/registeUser")
+	public String registeUserApi(UserForm f) {
+		User user = new User();
+		user.setUserName(f.getUserName());
+		user.setPassword(f.getPassword());
+		user.setFullName(f.getFullName());
+		userRepos.saveAndFlush(user);
+		
+		return "adminindex";
+	}
+
+}
